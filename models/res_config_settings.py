5bacfa21e959af2e71a143a1b605d105a7a1784c
jchanme <39323414+jchanme@users.noreply.github.com>
2018-11-24 16:43:22 +0800

    Update res_config_settings.py

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -1,6 +1,6 @@
 from odoo import fields, models
 
-class CompanyInfo(models.Model):
+class ResConfigSettings1(models.Model):
     _inherit = 'res.config.settings'
     
     sale_note = fields.Html(related='company_id.sale_note', string="Terms & Conditions")
