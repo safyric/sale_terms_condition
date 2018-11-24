721fdaf6077a191a773ea8bf82ae999881d14e2f
jchanme <39323414+jchanme@users.noreply.github.com>
2018-11-24 16:30:09 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Translate Company Information",
+    "version": "1.0",
+    "depends": ['base',
+                ],
+    'author': 'Safyric Co., Ltd.',
+    'license': 'AGPL-3',
+    'installable': True,
+}
