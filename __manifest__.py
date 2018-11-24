c4dd332b3906809bc5f05fcb10c9a38858c3e6a4
jchanme <39323414+jchanme@users.noreply.github.com>
2018-11-24 16:30:39 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,6 +2,8 @@
     "name": "Translate Company Information",
     "version": "1.0",
     "depends": ['base',
+                'crm',
+                'sale',
                 ],
     'author': 'Safyric Co., Ltd.',
     'license': 'AGPL-3',
