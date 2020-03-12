13e45d01e679377c29b3ba11d441865c962cfe4c
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-12 10:51:23 +0800

    Update res_config_settings.py

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResConfigSettings1(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    sale_note = fields.Html(related='company_id.sale_note', string="Terms & Conditions")
+    sale_note = fields.Html(related='company_id.sale_note', string="Terms & Conditions", readonly=False)
