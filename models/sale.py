02242f6f6e1d82bda9b2fda2b2bc4702dd4cc9e1
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-13 15:34:02 +0800

    Update sale.py

--- models/sale.py
+++ models/sale.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    note = fields.Html('Terms and conditions')
+    note = fields.Html(string='Terms and conditions', default=_default_note)
